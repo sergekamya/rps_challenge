57755e1f98f0a62de13615757a71dbf6e8b852d6
serge_kamya <sergekamya@gmail.com>
2021-09-25 12:26:11 +0200

    added code for random number generator in App.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+  const [userscore, setUserscore] = useState([]);
+  const [computerscore, setComputerscore] = useState([]);
+
+  function randomscore(max, min) {
+    return Math.floor(Math.random() * (max - min + 1) + 1);
+  }
+  randomscore(1, 3);
+
+
+
+
+
   return (
     <div>
       <h1 data-cy="header">Rock, paper, scissor game</h1>
       <button data-cy="button1">Rock</button>
       <button data-cy="button2">Paper</button>
       <button data-cy="button3">Scissor</button>
+      <p> {computerscore}</p>
     </div>
   );
 };
